58fcffd188f761017105004cfa55d8d1a0e96ede
morganherlocker <morgan.herlocker@gmail.com>
2014-12-12 10:53:37 -0500

    ;s

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-distance = require('turf-distance')
+distance = require('turf-distance');
 
 module.exports = function(targetPoint, points){
-  var nearestPoint
-  var count = 0
-  var dist = Infinity
+  var nearestPoint;
+  var count = 0;
+  var dist = Infinity;
   points.features.forEach(function(pt){
     if(!nearestPoint){
-      nearestPoint = pt
-      var dist = distance(targetPoint, pt, 'miles')
-      nearestPoint.properties.distance = dist 
+      nearestPoint = pt;
+      var dist = distance(targetPoint, pt, 'miles');
+      nearestPoint.properties.distance = dist;
     }
     else{
-      var dist = distance(targetPoint, pt, 'miles')
+      var dist = distance(targetPoint, pt, 'miles');
       if(dist < nearestPoint.properties.distance){
-        nearestPoint = pt
-        nearestPoint.properties.distance = dist
+        nearestPoint = pt;
+        nearestPoint.properties.distance = dist;
       }
     }
-  })
-  delete nearestPoint.properties.distance
-  return nearestPoint
+  });
+  delete nearestPoint.properties.distance;
+  return nearestPoint;
 }
