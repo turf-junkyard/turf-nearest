961904e6d021bac7a5413a928d1d038e8ce73bfa
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-20 11:29:59 -0800

    Update example and move to Istanbul

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ var distance = require('turf-distance');
  *
  * @module turf/nearest
  * @param {Point} point the reference point
- * @param {FeatureCollection} against a collection of {@link Feature|features}
- * with {@link Point} geometries
- * @returns {Feature} the closest point feature to point
+ * @param {FeatureCollection} against a FeatureCollection of Point features
+ * @return {Feature} the closest Point feature in `against` to `point`
+ * @example
+ * var point = turf.point(28.965797, 41.010086);
+ * point.properties['marker-color'] = '#0f0';
+ * var against = turf.featurecollection([
+ *  turf.point(28.973865, 41.011122),
+ *  turf.point(28.948459, 41.024204),
+ *  turf.point(28.938674, 41.013324)
+ * ]);
+ *
+ * var nearest = turf.nearest(point, against);
+ * nearest.properties['marker-color'] = '#f00';
+ *
+ * var result = turf.featurecollection(
+  against.features.concat(point));
+ *
+ * //=result
  */
 module.exports = function(targetPoint, points){
   var nearestPoint;
